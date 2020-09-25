cf29cb5def90285faac8b41f2534a52e96a461ff
Ligalaiz <slavakuz88@gmail.com>
2020-09-25 15:52:29 +0300

    feat: implement print function

--- script.js
+++ script.js
@@ -12,6 +12,21 @@ class Calculator {
     this.TempSign = options.TempSign;
     this.clear();
   }
+
+  print = (num) => {
+    if (this.TempFlag) {
+      this.currentOperand.value = num;
+      this.TempFlag = false;
+    } else {
+      this.currentOperand.value =
+        this.currentOperand.value === 'invalid in'
+          ? 0
+          : this.currentOperand.value;
+      this.currentOperand.value === '0'
+        ? (this.currentOperand.value = num)
+        : (this.currentOperand.value += num);
+    }
+  };
 }
 
 const digits = document.querySelectorAll('.number'),
@@ -23,3 +38,9 @@ const digits = document.querySelectorAll('.number'),
   sqrt = document.getElementById('sqrt');
 
 const calculator = new Calculator(currentOperand);
+
+digits.forEach((digit) => {
+  digit.addEventListener('click', (e) =>
+    calculator.print(e.target.textContent)
+  );
+});
