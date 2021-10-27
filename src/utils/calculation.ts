602cae8268159d57017efad122eb2c2fccec7fd7
Ligalaiz <slavakuz88@gmail.com>
2021-10-27 18:42:03 +0300

    feat: implement calculation functions

---
+++ src/utils/calculation.ts
@@ -0,0 +1,38 @@
+export const sum = (a: number, b: number): number => {
+  return (a * 100 + b * 100) / 100;
+};
+export const subtract = (a: number, b: number): number => {
+  return Number(((a * 100 - b * 100) / 100).toFixed(2));
+};
+export const multiply = (a: number, b: number): number => {
+  return (a * 100 * b * 100) / 10000;
+};
+export const divide = (a: number, b: number): number => {
+  return Number((a / b).toFixed(2));
+};
+
+interface ISign {
+  [key: string]: (a: number, b: number) => number;
+}
+
+interface IPriorities {
+  [key: string]: number;
+}
+
+export const signMap: ISign = {
+  '+': sum,
+  '-': subtract,
+  '/': divide,
+  '*': multiply,
+};
+
+export const [FIRST, SECOND] = [1, 2];
+
+export const prioritiesMap: IPriorities = {
+  '+': SECOND,
+  '-': SECOND,
+  '/': FIRST,
+  '*': FIRST,
+};
+
+export type ParsedLineType = (number | string)[];
