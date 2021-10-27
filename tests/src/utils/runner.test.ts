1a3c1cc0a029d08b735a35d10d80749fd8e448cb
Ligalaiz <slavakuz88@gmail.com>
2021-10-27 18:52:52 +0300

    feat: implement tests for runner function

---
+++ tests/src/utils/runner.test.ts
@@ -0,0 +1,13 @@
+import runner from '../../../src/utils/runner';
+
+describe('Check runner function', () => {
+  test('2 + 3 * 4 / 5 - 1 => 3.4', () => {
+    expect(runner('2 + 3 * 4 / 5 - 1')).toBe(3.4);
+  });
+  test('2 + 3 - 4 + 5 - 1 => 3.4', () => {
+    expect(runner('2 + 3 - 4 + 5 - 1')).toBe(5);
+  });
+  test('2 + 3 * 4 / 5 - 1 => 3.4', () => {
+    expect(runner('7 - 3 - 4 - 5 - 1')).toBe(-6);
+  });
+});
