8a185ef5ef8b85012c139ba93ce91347265cc66c
Ligalaiz <slavakuz88@gmail.com>
2021-10-27 18:53:21 +0300

    feat: implement runner function

---
+++ src/utils/runner.ts
@@ -0,0 +1,20 @@
+import parser from './parser';
+import { firstPriorities, secondPriorities } from './engine';
+
+const runner = (str: string): number => {
+  const stack = parser(str);
+
+  if (stack === null) {
+    throw new TypeError('Unexpected string');
+  }
+
+  const firstPrioritiesRes = firstPriorities(stack);
+
+  if (firstPrioritiesRes.length === 1) {
+    return Number(firstPrioritiesRes[0]);
+  }
+
+  return secondPriorities(firstPrioritiesRes);
+};
+
+export default runner;
