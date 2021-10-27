33fb53fb757bcf75db477cd6b58fe939c96ba703
Ligalaiz <slavakuz88@gmail.com>
2021-10-27 18:48:57 +0300

    feat: implement tests for parser function

---
+++ tests/src/utils/parser.test.ts
@@ -0,0 +1,15 @@
+import parser from '../../../src/utils/parser';
+
+describe('Check parser function', () => {
+  const result = [1, '+', 2, '-', 3, '*', 4, '/', 5];
+
+  test("1 + 2 - 3 => [1, '+', 2, '-', 3]", () => {
+    expect(parser('1 + 2 - 3')).toEqual([1, '+', 2, '-', 3]);
+  });
+  test("3 * 4 / 5 => [3, '*', 4, '/', 5]", () => {
+    expect(parser('3 * 4 / 5')).toEqual([3, '*', 4, '/', 5]);
+  });
+  test("1 + 2 - 3 * 4 / 5 => [1, '+', 2, '-', 3, '*', 4, '/', 5]", () => {
+    expect(parser('1 + 2 - 3 * 4 / 5')).toEqual(result);
+  });
+});
