c2d8c45d30092b38dd46f9850f20578a1ac71492
Ligalaiz <slavakuz88@gmail.com>
2021-11-02 20:39:01 +0300

    refactor: add more btn

--- src/stories/components/Button.stories.tsx
+++ src/stories/components/Button.stories.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import Button from '../../components/button';
+import { action } from '@storybook/addon-actions';
+import { Button } from '../../components/button';
+import { PropsButton } from '../../interfaces';
 
-export default {
+const meta: Meta = {
   title: 'Button',
   component: Button,
   argTypes: {
+    onClick: {
+      action: 'clicked',
+    },
     sign: {
-      defaultValue: 'Default value',
-    }
-  }
-} as Meta<typeof Button>;
+      defaultValue: '7',
+    },
+  },
+};
 
-const Template: Story<typeof Button> = (args) => <Button {...args} />;
+export default meta;
+const Template: Story<PropsButton> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  sign: '2',
+  onClick: action('default button'),
+};
 
 export const Primary = Template.bind({});
 Primary.args = {
-  sign: 1
-}
+  variant: 'primary',
+  sign: '2',
+  onClick: action('primary button'),
+};
