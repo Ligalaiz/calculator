cbf38501acf7ffd510ff86145c6200af4c3513a5
Ligalaiz <slavakuz88@gmail.com>
2021-11-02 20:31:19 +0300

    feat: add button tests

--- tests/src/components/Button.test.tsx
+++ tests/src/components/Button.test.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-import App from '../../../src/App';
+import App from '../../../src/components/App';
 
-test('renders btn', () => {
-  const { getByText } = render(<App />);
-  const buttonElement = getByText(/^\d{1}/i);
-  expect(buttonElement).toBeInTheDocument();
+describe('Check renders btn', () => {
+  test('renders Calculate btn', () => {
+    const { getByTestId } = render(<App />);
+    const buttonElement = getByTestId(/Calculate/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+  test('renders 1 btn', () => {
+    const { getByText } = render(<App />);
+    const buttonElement = getByText(/1/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+  test('renders ! btn', () => {
+    const { getByText } = render(<App />);
+    const buttonElement = getByText(/!/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
+
+describe('Check working btn', () => {
+  test('renders Calculate btn', () => {
+    const { getByTestId } = render(<App />);
+    const displayElement = getByTestId(/display/i);
+    expect(displayElement).toHaveValue('');
+    const buttonElement = getByTestId(/1/i);
+    expect(buttonElement).toBeInTheDocument();
+    userEvent.click(buttonElement);
+    expect(displayElement).toHaveValue('1');
+  });
 });
