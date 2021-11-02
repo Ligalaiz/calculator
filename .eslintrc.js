ffb23f3dc1f7232b416fcd400c17dca392dd2f78
Ligalaiz <slavakuz88@gmail.com>
2021-11-02 20:26:43 +0300

    fix: change config

--- .eslintrc.js
+++ .eslintrc.js
@@ -97,6 +97,14 @@ module.exports = {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+
+        // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
+
+        // use <root>/path/to/folder/tsconfig.json
+        project: './tsconfig.json',
+      },
     },
     propWrapperFunctions: [
       // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,12 @@ module.exports = {
   },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+      },
+    },
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-actions',
